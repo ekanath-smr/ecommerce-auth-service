bcecaeeb9a2a5593be3e595311bdd918b8a869ea
ekanath-smr <smrekanath@gmail.com>
2026-03-11 13:31:53 +0530

    Initial Auth Service project setup

---
+++ src/test/java/com/example/ecommerce_auth_service/EcommerceAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
